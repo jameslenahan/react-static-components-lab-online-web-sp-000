75f3b673c87415720d0374e40a3ff379c395945d
James Lenahan <james.k.lenahan@gmail.com>
2020-04-12 17:16:09 +0000

    Automatically backed up by Learn

--- src/App.js
+++ src/App.js
@@ -5,11 +5,10 @@ import MouseComponent from './MouseComponent'
 
 class App extends Component {
 	render() {
-		// your code in the return statement below!
 		return (
 			<div className="App">
 				<CatComponent />
-				<EinsteinQuoteComponent />
+				<GraceHopperQuoteComponent />
 				<MouseComponent />
 			</div>
 		);
